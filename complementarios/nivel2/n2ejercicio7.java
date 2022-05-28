46ecc717a02ab9755f7ebcd4cbd6c38ce1ae2683
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-05-28 16:35:22 -0300

    continuar con el ultimo ejercicio

--- complementarios/nivel2/n2ejercicio7.java
+++ complementarios/nivel2/n2ejercicio7.java
@@ -1,5 +1,7 @@
 package complementarios.nivel2;
 
 public class n2ejercicio7 {
-    
+    public static void main(String[] args) {
+        System.out.println("Luego continuo con el ejercicio 7 nivel 2");
+    }
 }
