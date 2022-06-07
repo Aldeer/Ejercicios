036b622b2ceb465855429f6c1bd91f5e2fc8738d
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-06-07 11:23:56 -0300

    primer ejercicio del nivel 3 terminado

--- complementarios/nivel3/Ejercicio1N3.java
+++ complementarios/nivel3/Ejercicio1N3.java
@@ -1,8 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 
 public class Ejercicio1N3 {
     public static void main(String[] args) {
+        //defino mi interface list para guardar strings
         List<String> palabras = new ArrayList<>();
+        //cargo mi lista de palabras
+        palabras.add("Hola");
+        palabras.add(null);
+        palabras.add("Informatorio");
+        palabras.add("");
+
+        /* 
+        usamos stream para filtrar los datos segun haya elementos nulos y o espacios vacios
+        devolviendo solo palabras
+        */
+        palabras.stream()
+            .filter(Objects::nonNull)
+            .filter(p -> !p.equals(""))
+            .forEach(p -> {
+                System.out.printf("%s ",p);
+            });
     }
 }
